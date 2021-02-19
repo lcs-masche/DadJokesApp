7ff404b5d0255bdcd56d3306cee236b4b2a84702
Moritz Asche <moritz@asches.de>
2021-02-19 11:14:23 -0500

    Initial Commit

---
+++ DadJokesApp/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  DadJokesApp
+//
+//  Created by Mo Asche on 2021-02-19.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ DadJokesApp/DadJokesAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  DadJokesAppApp.swift
+//  DadJokesApp
+//
+//  Created by Mo Asche on 2021-02-19.
+//
+
+import SwiftUI
+
+@main
+struct DadJokesAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
